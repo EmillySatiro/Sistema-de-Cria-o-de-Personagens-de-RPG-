8f69d87fd13c72ef0ad42ebcd9b94c3f44679cfb
EmillySatiro <sousasatiroclaraemilly@gmail.com>
2025-05-30 12:36:09 -0300

    Atualização das raças e classe, e mensagens de erro

--- src/main.hs
+++ src/main.hs
@@ -7,7 +7,12 @@ module Main where
 import Personagem
 import Nomeavel
 import System.IO
-import Data.Char (toUpper)
+import Data.Char (toUpper, toLower, isSpace)
+
+-- Função auxiliar para remover espaços no início e fim
+trim :: String -> String
+trim = f . f
+  where f = reverse . dropWhile isSpace
 
 -- Função para mostrar todos os personagens com descrição completa
 mostrarPersonagens :: [Personagem] -> IO ()
@@ -56,35 +61,60 @@ menu ps = do
         mostrarPersonagens ps
         menu ps
       "2" -> do
+        putStrLn "Caso você escreva um valor invalido nas opções abaixo, seu personagem receberá as configurações padrão (Guerreiro/Humano)"
         putStr "Nome: "; hFlush stdout; n <- getLine
         putStrLn "Escolha a Classe:"
-        putStrLn "0. Guerreiro"
-        putStrLn "1. Mago"
-        putStrLn "2. Ladino"
-        putStrLn "3. Clérigo"
-        putStrLn "4. Arqueiro"
+        putStrLn "0. Barbaro"
+        putStrLn "1. Bardo"
+        putStrLn "2. Bruxo"
+        putStrLn "3. Clerigo"
+        putStrLn "4. Druida"
+        putStrLn "5. Feiticeiro"
+        putStrLn "6. Guerreiro"
+        putStrLn "7. Ladino"
+        putStrLn "8. Mago"
+        putStrLn "9. Monge"
+        putStrLn "10. Paladino"
+        putStrLn "11. Patrulheiro"
         putStr "Digite o número da classe: "; hFlush stdout; cIdx <- readLn
         let classe' = case cIdx of
-                        0 -> Guerreiro
-                        1 -> Mago
-                        2 -> Ladino
+                        0 -> Barbaro
+                        1 -> Bardo
+                        2 -> Bruxo
                         3 -> Clerigo
-                        4 -> Arqueiro
+                        4 -> Druida
+                        5 -> Feiticeiro
+                        6 -> Guerreiro
+                        7 -> Ladino
+                        8 -> Mago
+                        9 -> Monge
+                        10 -> Paladino
+                        11 -> Patrulheiro
                         _ -> Guerreiro -- valor padrão
+
         putStrLn "Escolha a Raça:"
         putStrLn "0. Humano"
         putStrLn "1. Elfo"
         putStrLn "2. Anão"
-        putStrLn "3. Orc"
-        putStrLn "4. Goblin"
+        putStrLn "3. Halfling"
+        putStrLn "4. Meio-Orc"
+        putStrLn "5. Meio-Elfo"
+        putStrLn "6. Gnomo"
+        putStrLn "7. Draconato"
+        putStrLn "8. Tiefling"
         putStr "Digite o número da raça: "; hFlush stdout; rIdx <- readLn
         let raca' = case rIdx of
                       0 -> Humano
                       1 -> Elfo
-                      2 -> Anao
-                      3 -> Orc
-                      4 -> Goblin
+                      2 -> Anão
+                      3 -> Halfling
+                      4 -> MeioOrc
+                      5 -> MeioElfo
+                      6 -> Gnomo
+                      7 -> Draconato
+                      8 -> Tiefling
                       _ -> Humano -- valor padrão
+
         putStr "Força: "; hFlush stdout; f <- readLn
         putStr "Inteligência: "; hFlush stdout; i <- readLn
         putStr "Destreza: "; hFlush stdout; d <- readLn
@@ -92,12 +122,21 @@ menu ps = do
         menu (adicionarPersonagem novo ps)
       "3" -> do
         putStr "Nome do personagem a remover: "; hFlush stdout; n <- getLine
-        menu (removerPersonagemMain n ps)  -- Usando a função renomeada
+        let nomeNormalizado = map toLower (trim n)
+        if any (\p -> map toLower (trim (obterNome p)) == nomeNormalizado) ps
+          then do
+            let ps' = removerPersonagemMain n ps
+            putStrLn "Personagem removido com sucesso!"
+            menu ps'
+          else do
+            putStrLn "Erro: Personagem não encontrado!"
+            menu ps
       "4" -> do
         putStr "Nome do personagem: "; hFlush stdout; n <- getLine
         putStr "Nome do item: "; hFlush stdout; ni <- getLine
         putStr "Descrição do item: "; hFlush stdout; di <- getLine
-        if any (\p -> obterNome p == n) ps
+        let nomeNormalizado = map toLower (trim n)
+        if any (\p -> map toLower (trim (obterNome p)) == nomeNormalizado) ps
           then do
             let ps' = adicionarItemPersonagem ni n di ps
             putStrLn "Item adicionado com sucesso!"
@@ -108,8 +147,24 @@ menu ps = do
       "5" -> do
         putStr "Nome do personagem: "; hFlush stdout; n <- getLine
         putStr "Nome do item a remover: "; hFlush stdout; ni <- getLine
-        let ps' = [if obterNome p == n then removerItem ni p else p | p <- ps]
-        menu ps'
+        let nomeNormalizado = map toLower (trim n)
+
+        case filter (\p -> map toLower (trim (obterNome p)) == nomeNormalizado) ps of
+          [] -> do
+            putStrLn "Erro: Personagem não encontrado!"
+            menu ps
+          (pEncontrado:_) ->
+            let personagemAtualizado = removerItem ni pEncontrado
+            in if personagemAtualizado == pEncontrado
+                then do
+                  putStrLn "Erro: Item não encontrado no personagem!"
+                  menu ps
+                else do
+                  let ps' = map (\p -> if map toLower (trim (obterNome p)) == nomeNormalizado
+                                        then personagemAtualizado
+                                        else p) ps
+                  putStrLn "Item removido com sucesso!"
+                  menu ps'
       "6" -> do
         writeFile "personagens.txt" (show ps)
         putStrLn "\nPersonagens salvos com sucesso!"
